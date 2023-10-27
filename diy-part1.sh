f22494e2e1583c05c62a1d1047c6fd555459bad1
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2023-10-28 00:09:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,3 @@
 
 # Uncomment a feed source
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-#sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial/g' feeds/luci/collections/luci/Makefile
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
